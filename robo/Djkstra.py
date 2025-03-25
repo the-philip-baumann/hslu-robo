e279c09419e7dc46174dc4542952e0063424c08b
Philip Baumann <philip.baumann@hispeed.ch>
2025-03-25 17:17:44 +0100

    Add interface for Dijstra

---
+++ robo/Djkstra.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Instruction(Enum):
+    STRAIGHT_FORWARD = 0
+    RIGHT = 1
+    LEFT = 2
+    
+    
+
+def caluclate_instruction_set() -> list[Instruction]:
+    pass
